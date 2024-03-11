15cfb1575aa6b04705b41755c0f5084c24c25df5
naylatanjung <naylatanjung79@gmail.com>
2024-03-11 18:11:10 +0700

    tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content" style="position: fixed; bottom: 0; width: 100%;">
+  <aside>
+    <p>Copyright © 2024 - All rights reserved by Nayla Az Zahra</p>
+  </aside>
+</footer>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar bg-green-500 bg-base-100">
+    <div class="navbar-start">
+    </div>
+    <div class="navbar-center">
+      <a href="/" class="btn btn-ghost text-xl hover:bg-transparent">To Do List</a>
+    </div>
+    <div class="navbar-end">
+    </div>
+  </div>
+  
